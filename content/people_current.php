5a4013b5b279e82d4a27720f80e19d35050d8a33
RafiKueng <rafi.kueng@gmx.ch>
2013-12-09 19:07:42 +0100

    better peoplelist

--- content/people_current.php
+++ content/people_current.php
@@ -5,19 +5,20 @@ $lines = read_csv("people_current.csv");
 echo <<<END
 <article>
   <h2>People current</h2>
-  <ul id="peoplelist current">
+  <ul class="peoplelist current">
 END;
 
 
 foreach($lines as $l){
-  $str = "    <li>";
-  $str .= (isset($l[3]) ? '<a class="homepage" href="'.$l[3].'">' : '');
+  $str =  '    <li><span class="titlename">';
+  $str .= (strlen($l[3])>0 ? '<a class="homepage" href="'.$l[3].'">' : '');
   $str .= '<span class="title">'.(strlen($l[0])>0 ? $l[0] . " " : "").'</span>';
-  $str .= '<span class="name">'.$l[1].'</span>';
-  $str .= (isset($l[3]) ? '</a>' : '');
-  $str .= '<br>';
-  $str .= '<span class="room">'.$l[2].'</span> ';
-  $str .= '<a class="email" href="mailto:'.$l[4].'">'.$l[4].'</a></li>';
+  $str .= '<span class="name">'.$l[1].'</span></span>';
+  $str .= (strlen($l[3])>0 ? '</a>' : '');
+  #$str .= '<br>';
+  $str .= '<a class="email" href="mailto:'.$l[4].'">'.$l[4].'</a>';
+  $str .= '<span class="room">'.$l[2].'</span>';
+  $str .= '</li>';
   echo $str;
 }
 
--- content/people_older.php
+++ content/people_older.php
@@ -5,14 +5,14 @@ $lines = read_csv("people_older.csv");
 echo <<<END
 <article>
   <h2>People older</h2>
-  <ul id="peoplelist old">
+  <ul class="peoplelist old">
 END;
 
 
 foreach($lines as $l){
-  $str = "    <li>";
+  $str =  '    <li><span class="titlename">';
   $str .= '<span class="title">'.(strlen($l[0])>0 ? $l[0] . " " : "").'</span>';
-  $str .= '<span class="name">'.$l[1].'</span> ';
+  $str .= '<span class="name">'.$l[1].'</span></span>';
   $str .= '<a class="email" href="mailto:'.$l[3].'">'.$l[3].'</a>';
   $str .= '<span class="leavedate"> '.$l[2].'</span> ';
   $str .= '<span class="leavedest">'.(strlen($l[4])>0 ? "(".$l[4] . ")" : "").'</span></li>';
